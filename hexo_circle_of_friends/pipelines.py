24e3968884fbce10e666d48316f17b2c901e8c9d
noionion <noionion@outlook.com>
2022-02-08 13:16:32 +0800

    hope sqlite can use

--- hexo_circle_of_friends/pipelines.py
+++ hexo_circle_of_friends/pipelines.py
@@ -20,7 +20,7 @@ class HexoCircleOfFriendsPipeline:
 
         # 友链去重
         self.friend_set = set()
-        with open(".hexo_circle_of_friends/config/link.yml",  "r", encoding="utf-8") as f:
+        with open("./hexo_circle_of_friends/config/link.yml",  "r", encoding="utf-8") as f:
             self.friends = yaml.load(f.read())
         for friend in self.friends:
             if friend["link"] not in self.friend_set:
--- hexo_circle_of_friends/spiders/hexo_circle_of_friends.py
+++ hexo_circle_of_friends/spiders/hexo_circle_of_friends.py
@@ -22,7 +22,7 @@ class FriendpageLinkSpider(scrapy.Spider):
 
     def start_requests(self):
         # 从友链配置文件 ./config/link.yml 导入友链列表
-        with open(".hexo_circle_of_friends/config/link.yml",  "r", encoding="utf-8") as f:
+        with open("./hexo_circle_of_friends/config/link.yml",  "r", encoding="utf-8") as f:
             friends = yaml.load(f.read())
         for friend in friends:
             self.friend_poor.put(friend)
